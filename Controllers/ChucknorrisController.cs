80cba67ec68603643b88a62173b15bf2946775ae
Abraham20 <31997221+Abraham-Wasswa@users.noreply.github.com>
2020-01-24 16:25:35 -0800

    1/24/2020
    conetrated on the middle tier

---
+++ Controllers/ChucknorrisController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SimpleWebApp.Models;
+using SimpleWebApp1.SimpleUI;
+
+namespace SimpleWebApp1.Controllers
+{
+    public class ChucknorrisController : Controller
+    {
+        public ChuckNorisJokes SelectCategories()
+        {
+            ChuckNorisJokes chuckNorisJokes = new ChuckNorisJokes();
+            chuckNorisJokes.Categories = new List<string>();
+
+            chuckNorisJokes.Categories.Add("animal");
+            chuckNorisJokes.Categories.Add("Career");
+            chuckNorisJokes.Categories.Add("Celebrity");
+            chuckNorisJokes.Categories.Add("dev");
+            chuckNorisJokes.Categories.Add("explicit");
+            chuckNorisJokes.Categories.Add("fashion");
+            chuckNorisJokes.Categories.Add("food");
+            chuckNorisJokes.Categories.Add("history");
+            chuckNorisJokes.Categories.Add("money");
+            chuckNorisJokes.Categories.Add("movie");
+            chuckNorisJokes.Categories.Add("music");
+            chuckNorisJokes.Categories.Add("political");
+            chuckNorisJokes.Categories.Add("religion");
+            chuckNorisJokes.Categories.Add("science");
+            chuckNorisJokes.Categories.Add("sports");
+            chuckNorisJokes.Categories.Add("travel");
+
+            return chuckNorisJokes;
+
+        }
+        public IActionResult Index()
+        {
+            ChuckNorisJokes chuckNorisJokes = SelectCategories();
+            return View(chuckNorisJokes);
+        }
+
+        [HttpPost]
+        public ActionResult Index(ChuckNorisJokes chuckNorisJokes, string Categories)
+        {
+            chuckNorisJokes = SelectCategories();
+
+            if(Categories != null)
+            {
+                /*Calling the API for chucknorris.io*/
+                string apiKey = "CHUCK API";
+                HttpWebRequest apiRequest = WebRequest.Create("https://api.chucknorris.io/Jokes/random?" + Categories + "={category}" + apiKey) as HttpWebRequest;
+
+                string apiResponse = "";
+                using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    apiResponse = reader.ReadToEnd();
+                }
+                //End 
+
+                //JSON to translate to CSharp
+                Randomm category = JsonConvert.DeserializeObject<Randomm>(apiResponse);
+
+                StringBuilder b = new StringBuilder();
+                b.Append("<table><tr><th> Chuck </th></tr>");
+                b.Append("<tr><td>id</td><td>" + category.id + "</td></tr>" );
+                b.Append("<tr><td>id</td><td>" + category.IconUrl + "</td></tr>");
+                b.Append("<tr><td>id</td><td>" + category.Url + "</td></tr>");
+                b.Append("<tr><td>id</td><td>" + category.Value + "</td></tr>");
+                b.Append("</table");
+                chuckNorisJokes.apiResponse = b.ToString();
+            }
+
+            return View(chuckNorisJokes);
+        }
+        
+    }
+}
--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -14,25 +14,11 @@
 var Drop = new Vue({
     el: '#me',
     data: {
-        myModel: {},
-        myOption: {},
-        myOptions: [
-            { id: 1, Category: "animal" },
-            { id: 2, Category: "career" },
-            { id: 3, Category: "celebrity" },
-            { id: 4, Category: "dev" },
-            { id: 5, Category: "explicit" },
-            { id: 6, Category: "fashion" },
-            { id: 7, Category: "food" },
-            { id: 8, Category: "history" },
-            { id: 9, Category: "money" },
-            { id: 10, Category: "movie" },
-            { id: 11, Category: "music" },
-            { id: 12, Category: "political" },
-            { id: 13, Category: "religion" },
-            { id: 14, Category: "science" },
-            { id: 15, Category: "sport" },
-            { id: 16, Category: "travel" }
-        ]
+        List: '@Model'
+    },
+    methords: {
+        List: function () {
+            onclick(this.List);
+        }
     }
 });
